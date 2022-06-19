86de98f0f9d96133148b65fff4100947a37e6b87
palewire <b@palewi.re>
2022-06-19 11:27:35 -0700

    Updated some stuff

--- setup.py
+++ setup.py
@@ -7,15 +7,45 @@ def read(fname):
         return f.read()
 
 
+def version_scheme(version):
+    """
+    Version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    import time
+
+    from setuptools_scm.version import guess_next_version
+
+    if version.exact:
+        return version.format_with("{tag}")
+    else:
+        _super_value = version.format_next_version(guess_next_version)
+        now = int(time.time())
+        return _super_value + str(now)
+
+
+def local_version(version):
+    """
+    Local version scheme hack for setuptools_scm.
+
+    Appears to be necessary to due to the bug documented here: https://github.com/pypa/setuptools_scm/issues/342
+
+    If that issue is resolved, this method can be removed.
+    """
+    return ""
+
+
 setup(
     name='noaa-wildfires',
-    version='0.0.3',
     description="Download wildfires data from NOAA satellites",
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     author='Ben Welsh',
     author_email='b@palewi.re',
-    url='http://www.github.com/palewire/noaa-wildfires',
+    url='https://palewi.re/docs/noaa-wildfires',
     license="MIT",
     packages=("noaa_wildfires",),
     install_requires=[
@@ -28,18 +58,20 @@ setup(
         [console_scripts]
         noaawildfires=noaa_wildfires.cli:cmd
     """,
+    use_scm_version={"version_scheme": version_scheme, "local_scheme": local_version},
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'License :: OSI Approved :: MIT License',
     ],
     project_urls={
-        'Maintainer': 'https://github.com/palewire',
-        'Source': 'https://github.com/palewire/noaa-wildfires',
-        'Tracker': 'https://github.com/palewire/noaa-wildfires/issues'
+        'Maintainer': 'https://github.com/datadesk',
+        'Source': 'https://github.com/datadesk/noaa-wildfires',
+        'Tracker': 'https://github.com/datadesk/noaa-wildfires/issues'
     },
 )
